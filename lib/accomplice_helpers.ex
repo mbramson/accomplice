c28cd85fe42c4b3ccb1217b7ad32af6ded786ebe
Mathew Bramson <mathewbramson@gmail.com>
2017-11-08 23:09:54 -0500

    Add create_actions function

--- lib/accomplice_helpers.ex
+++ lib/accomplice_helpers.ex
@@ -1,9 +1,47 @@
 defmodule Accomplice.Helpers do
+  @moduledoc false
 
   @doc false
   @spec pop_random_element_from_list(list(any())) :: {any(), list(any())}
   def pop_random_element_from_list(list) do
     element_index = Enum.random(1..length(list)) - 1
     List.pop_at(list, element_index)
+  end
+
+  @doc false
+  @spec create_actions(list(list(any())), list(any()), map()) :: nonempty_list({:add, any()} | :complete) | :impossible
+  def create_actions(current_group, ungrouped, %{minimum: minimum, ideal: ideal, maximum: maximum}) do
+    current_group_length = length(current_group)
+    cond do
+      current_group_length < minimum ->
+        # when the current_group's length is less than the minimum constraint,
+        # our only option is to add en element. If there are no elements to add,
+        # then we have no legal action and so return the :impossible atom
+        case ungrouped do
+          []        -> :impossible
+          ungrouped -> add_actions(ungrouped)
+        end
+      current_group_length == maximum ->
+        # when the current_group's length is equal to the maximum constraint,
+        # our only option is to complete the group.
+        [:complete]
+      current_group_length < ideal ->
+        # when the current group's length is less than the ideal, we first want
+        # to try adding an element, then we'll try completing the group since we
+        # know that we're at least at the minimum length.
+        add_actions(ungrouped) ++ [:complete]
+      current_group_length == ideal ->
+        # when the current group's length is at ideal, we first want to try completing
+        # the group, then try adding the elements, since we know that we're below the
+        # maximum current_group length.
+        [:complete | add_actions(ungrouped)]
+    end
+  end
+
+  @spec add_actions(list(any())) :: list({:add, any()})
+  defp add_actions(ungrouped) do
+    ungrouped
+    |> Enum.shuffle
+    |> Enum.map(fn element -> {:add, element} end)
   end
 end
--- test/accomplice_helpers_test.exs
+++ test/accomplice_helpers_test.exs
@@ -1,8 +1,9 @@
-defmodule AccompliceHelpersTest do
+defmodule AccompliceHelpersTest do 
   use ExUnit.Case
   use Quixir
 
   alias Accomplice.Helpers
+  import OrderInvariantCompare # for <~> operator
 
   describe "pop_random_element_from_list/1" do
     test "returns an element and the rest of the list" do
@@ -16,6 +17,56 @@ defmodule AccompliceHelpersTest do
         if original_list |> Enum.uniq |> length == length(original_list) do
           assert element not in rest_of_list
         end
+      end
+    end
+  end
+
+  describe "create_actions/3" do
+    test "when there are no ungrouped elements left and we're above minimum, complete the group" do
+      assert [:complete] == Helpers.create_actions([1], [], %{minimum: 1, ideal: 1, maximum: 1})
+      assert [:complete] == Helpers.create_actions([1], [], %{minimum: 1, ideal: 3, maximum: 3})
+      assert [:complete] == Helpers.create_actions([1, 2], [], %{minimum: 2, ideal: 2, maximum: 2})
+      assert [:complete] == Helpers.create_actions([1, 2], [], %{minimum: 2, ideal: 3, maximum: 3})
+    end
+
+    test "when there are no ungrouped elements left and we're below minimum, return :impossible" do
+      assert :impossible == Helpers.create_actions([], [], %{minimum: 2, ideal: 2, maximum: 2})
+      assert :impossible == Helpers.create_actions([1], [], %{minimum: 3, ideal: 3, maximum: 3})
+    end
+
+    test "when the current_group is below minimum constraint, recommend adding an element to current group" do
+      assert [{:add, 1}] == Helpers.create_actions([], [1], %{minimum: 1, ideal: 1, maximum: 1})
+      assert [{:add, 1}] == Helpers.create_actions([], [1], %{minimum: 2, ideal: 2, maximum: 2})
+      assert [{:add, 2}] == Helpers.create_actions([1], [2], %{minimum: 2, ideal: 2, maximum: 2})
+      assert [{:add, 1}, {:add, 2}] <~> Helpers.create_actions([], [1, 2], %{minimum: 1, ideal: 1, maximum: 1})
+    end
+
+    test "when the current_group is at the maximum constraint, only action is to complete the group" do
+      assert [:complete] == Helpers.create_actions([1], [], %{minimum: 1, ideal: 1, maximum: 1})
+      assert [:complete] == Helpers.create_actions([1], [2], %{minimum: 1, ideal: 1, maximum: 1})
+      assert [:complete] == Helpers.create_actions([1, 2], [2], %{minimum: 1, ideal: 1, maximum: 2})
+    end
+
+    test "when the current_group is below ideal constraint, but equal or above minimum, add elements, then complete group" do
+      assert [{:add, 2}, :complete] == Helpers.create_actions([1], [2], %{minimum: 1, ideal: 2, maximum: 2})
+      assert [{:add, 3}, :complete] == Helpers.create_actions([1, 2], [3], %{minimum: 1, ideal: 3, maximum: 3})
+    end
+
+    test "when the current_group is at ideal constraint, but below the maximum constraint, complete, then add elements" do
+      assert [:complete, {:add, 2}] == Helpers.create_actions([1], [2], %{minimum: 1, ideal: 1, maximum: 2})
+      assert [:complete, {:add, 3}] == Helpers.create_actions([1, 2], [3], %{minimum: 1, ideal: 2, maximum: 3})
+      assert [:complete, {:add, 3}] == Helpers.create_actions([1, 2], [3], %{minimum: 2, ideal: 2, maximum: 3})
+      assert [:complete, {:add, 4}] == Helpers.create_actions([1, 2, 3], [4], %{minimum: 1, ideal: 3, maximum: 4})
+      assert [:complete, {:add, 4}] == Helpers.create_actions([1, 2, 3], [4], %{minimum: 2, ideal: 3, maximum: 4})
+      assert [:complete, {:add, 4}] == Helpers.create_actions([1, 2, 3], [4], %{minimum: 3, ideal: 3, maximum: 4})
+
+      assert [:complete | add_actions] = Helpers.create_actions([1], [2, 3, 4], %{minimum: 1, ideal: 1, maximum: 2})
+      assert add_actions <~> [{:add, 2}, {:add, 3}, {:add, 4}]
+    end
+
+    test "there are no invalid inputs" do
+      ptest [min: positive_int(), ideal: positive_int(), max: positive_int(), ungrouped: list(of: any(), min: 0, max: 10), current_group: list(of: any(), min: 0, max: 10)], repeat_for: 30 do
+        Helpers.create_actions(current_group, ungrouped, %{minimum: min, ideal: ideal, maximum: max})
       end
     end
   end
