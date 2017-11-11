b8defb7add49fdd25f6bd1ffc3710c3f6b12007f
Mathew Bramson <mathewbramson@gmail.com>
2017-11-10 23:44:29 -0500

    Fix up mix.exs for hex

--- mix.exs
+++ mix.exs
@@ -5,9 +5,14 @@ defmodule Accomplice.Mixfile do
     [
       app: :accomplice,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.3",
+      build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+      name: "Accomplice",
+      source_url: "https://github.com/mbramson/accomplice"
     ]
   end
 
@@ -21,8 +26,25 @@ defmodule Accomplice.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, "~> 0.18", only: :dev},
       {:order_invariant_compare, "~> 1.0", only: :test},
-      { :quixir, "~> 0.9", only: :test }
+      {:quixir, "~> 0.9", only: :test}
+    ]
+  end
+
+  defp description() do
+    """
+    Accomplice is a library for grouping members of a list with a respect to a
+    number of constraints.
+    """
+  end
+
+  defp package() do
+    [
+      files: ["lib", "mix.exs", "README.md", "LICENSE.md"],
+      maintainers: ["Mathew Bramson"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/mbramson/accomplice"}
     ]
   end
 end
